26bc5a6588027e290da632bd5d68cdec98b1f43b
eslammyaakoup <eslammyaakoup7090@gmail.com>
2024-04-12 20:46:01 +0200

    all files

---
+++ main.js
@@ -0,0 +1,15 @@
+// let arr = ["a", "b", "c", "d"];
+
+// console.log(arr.slice(2, 3));
+// let ul = document.createElement("ul");
+// let li = document.createElement("li");
+// // ul.appendChild(li);
+// // div.innerHTML = ul;
+// // console.log(div);
+// // console.log(div);
+// let div = document.getElementsByClassName("div");
+// setInterval(() => {
+//   let rondom = Math.floor(Math.random() * arr.length);
+//   div.innerText = "dddddervvtrbbdddgddddddwww";
+//   console.log(arr[rondom]);
+// }, 1000);
